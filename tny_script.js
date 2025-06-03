2faf81beae528987d3bf0e41b1f504a0054a1636
finteger <nashassociatesinc@gmail.com>
2025-06-03 11:18:55 -0600

    Update

--- tny_script.js
+++ tny_script.js
@@ -26,3 +26,11 @@ var timeStr = currentDay.toLocaleTimeString();
 document.getElementById("dateNow").innerHTML = 
 dateStr + "<br />" + timeStr;
 
+//Calculate the days until New Years Bash
+var newYear = new Date('January 1st, 2025');
+var nextYear = currentDay.getFullYear() + 1;
+newYear.setFullYear(nextYear);
+var daysLeft = (newYear - currentDay)/(1000*60*60*24);
+
+//Display the time left until New Years Eve
+document.getElementById("days").textContent = daysLeft;
