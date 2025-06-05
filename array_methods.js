3ff6f55fe15026f88a0396895e06d3c879fe6c9d
finteger <nashassociatesinc@gmail.com>
2025-06-05 10:06:10 -0600

    Update

---
+++ array_methods.js
@@ -0,0 +1,35 @@
+//array literal
+var array = [8, 5, 3, 4, 2, 6, 7, 1, 9, 10];
+
+//sort by alphabetic or numerical order
+var new_array_sorted = array.sort((a,b) => a - b);
+ 
+console.log(new_array_sorted);
+//console.log(array.reverse());
+
+
+//to add items to the end of an array 
+array.push(11);
+console.log(array); //output: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
+
+//remove the last element of an array
+array.pop();
+console.log(array); //output: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 
+
+
+//check if there is at least item in an array that matches our criteria
+console.log(array.some(num => num == 1)); //output: true
+
+//check if all the items in an array match a certain condition
+console.log(array.every(num => num > 0)); //output: true
+
+//reduces all items to one
+console.log(array.reduce((a, b) => a + b)); //output: 55
+
+//what if I want to join an array to an array
+console.log(array.concat(array2)); //output: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 
+
+//if we want to joint individual strings together into a single string
+let words = ["hello", "world", "!"]
+let sentence = words.join(' ');
+console.log(sentence); //output:  hello world !
---
+++ arrays.js
@@ -0,0 +1,17 @@
+//array literal
+//arrays are 0-based indexed
+var array = ['apple', 2.33, null, true, 'dog', 'cat', 23, 6];
+
+console.log(array[4]); // output: dog
+
+//reassignment of value
+array[4] = 'cat';
+
+console.log(array[4]);  //output: cat
+
+//call length on the array
+console.log(array.length); //output: 8
+
+
+
+
